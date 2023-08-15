18b1e5e1f3dd2057173404b04a2326ecedb3539b
HowlingPython <126528389+HowlingPython@users.noreply.github.com>
2023-08-15 11:15:22 -0300

    lo que hicimos recien

--- City.hs
+++ City.hs
@@ -5,6 +5,13 @@ import Point
 
 data City = Cit String Point deriving (Eq, Show)
 
+
+
 newC :: String -> Point -> City
+newC = Cit
+
 nameC :: City -> String
+nameC Cit s _ = s
+
 distanceC :: City -> City -> Float
+distanceC Cit _ _ = difP p0 p1
--- Point.hs
+++ Point.hs
@@ -1,8 +1,10 @@
-
-module Point ( Point, newP, difP)
+module Point (Point, newP, difP)
    where
 
 data Point = Poi Int Int deriving (Eq, Show)
 
 newP :: Int -> Int -> Point
+newP = Poi
+
 difP :: Point -> Point -> Float  -- distancia absoluta
+difP p p1 = sqrt((p)**2 + (p1)**2)
