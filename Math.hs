12069e1347467b4bae5963bd234ad42b9b80ee49
HowlingPython <126528389+HowlingPython@users.noreply.github.com>
2023-08-15 15:24:43 -0300

    econtre como hacer potencia asi que elimine el Math module

--- Math.hs
+++
@@ -1,5 +0,0 @@
-module Math (sqr)
-    where
-
-sqr :: Num a => a -> a
-sqr x = x * x
--- Point.hs
+++ Point.hs
@@ -1,13 +1,10 @@
 module Point (Point, newP, difP)
    where
 
-import Math
-
 data Point = Poi Int Int deriving (Eq, Show)
-
 
 newP :: Int -> Int -> Point
 newP = Poi
 
 difP :: Point -> Point -> Float  -- distancia absoluta
-difP (Poi x1 y1) (Poi x2 y2) = sqrt(fromIntegral (sqr(x2 - x1) + sqr(y2 - y1)))
+difP (Poi x1 y1) (Poi x2 y2) = sqrt(fromIntegral ((x2 - x1)^2 + (y2 - y1)^2))
