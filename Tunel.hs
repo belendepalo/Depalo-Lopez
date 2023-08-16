af202d109e29929c120920d4873d44387e230f04
HowlingPython <126528389+HowlingPython@users.noreply.github.com>
2023-08-16 11:31:35 -0300

    Update Tunel.hs

--- Tunel.hs
+++ Tunel.hs
@@ -10,12 +10,15 @@ import Link
 data Tunel = Tun [Link] deriving (Eq, Show)
 
 newT :: [Link] -> Tunel
-
+newT = Tun
 
 connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conceta estas dos ciudades distintas
+connectsT c0 c1 (Tun links) = (head links == c0 && last links == c1) || (head links  == c1 && last links  == c0) 
 
+-- creo que va a ser util [x | x <- xs, ]
 
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
-
+usesT l_0 (Tun links) = l_0 `elem` links 
 
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
+delayT (Tun links) = sum $ map delayL links 
