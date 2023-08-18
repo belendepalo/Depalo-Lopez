6ca2ad881f1c9cf8a647f0c2d838b32b8ae7f29a
HowlingPython <126528389+HowlingPython@users.noreply.github.com>
2023-08-18 14:40:14 -0300

    cambie linkedR

--- Region.hs
+++ Region.hs
@@ -13,23 +13,24 @@ newR :: Region
 newR = Reg [] [] []
 
 foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
-foundR (Reg cities _ _) newCity | newCity `elem` cities = error "The City is already in the Region" 
-                                | otherwise = Reg (newCity:cities) _ _ 
+foundR (Reg cities _ _) newCity
+   | newCity `elem` cities = error "The City is already in the Region"
+   | otherwise = Reg (newCity:cities) _ _
 
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
-linkR (Reg cities links tunnels) c0 c1 quality 
+linkR (Reg cities links tunnels) c0 c1 quality
    | c0 == c1 = error "Cannot link a city to itself"
    | c0 `notElem` cities || c1 `notElem` cities = error "One or both cities are not in the region"
-   | otherwise = Reg cities ((newL c0 c1 quality):links) tunnels
+   | otherwise = Reg cities (newL c0 c1 quality:links) tunnels
 
 tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
-tunelR (Reg cities links tunnels) 
+tunelR (Reg cities links tunnels)
 
 connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
 connectedR (Reg _ _ tunnels) c0 c1 = any (connectsT c0 c1) tunnels
 
 linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
-linkedR (Reg _ links _) c0 c1 = foldr (/link acc -> acc || linksL c0 c1 link) False links 
+linkedR (Reg _ links _) c0 c1 = any (\link -> linksL c0 c1 link || linksL c1 c0 link) links
 
 delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
 delayR (Reg _ links _) c0 c1 = delayL link
