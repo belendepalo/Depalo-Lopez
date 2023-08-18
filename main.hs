7792b32e596d8c38da790487daf449386ecdd66d
HowlingPython <126528389+HowlingPython@users.noreply.github.com>
2023-08-18 20:16:28 -0300

    comentario de como compilar

--- main.hs
+++ main.hs
@@ -1,5 +1,14 @@
 module Main where
--- el comando para compilar es: ghc -odir build/o_files -hidir build/hi_files -o pruebas main.hs ; ./pruebas
+
+{-
+el comando para compilar es: ghc -odir build/o_files -hidir build/hi_files -o pruebas main.hs
+para ejecutarlo es: ./pruebas
+y para las carpetas donde se guardan los archivos basura son los tres siguentes:
+mkdir build
+mkdir build/o_files
+mkdir build/hi_files
+-} 
+
 import Pruebas
 
 main :: IO ()
