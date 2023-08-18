7d36e20ee15802712bca1fe2b55b16774144c17f
HowlingPython <126528389+HowlingPython@users.noreply.github.com>
2023-08-18 14:02:12 -0300

    Update Region.hs
    en newR lo cambie para que tenga listas vacias que se van a llenar a medida que usemos mas funciones (seria el caso base de cunado inicializas una Region)
    
    en linkR cambie para que chequee si: la ciudades estan en la region y si metio la misma ciudad dos veces, y si no salta ninguno de esos, crea esos links con la region actualizada. no importa que halla ignorado el tunnels en el costructor pero lo tengo que poner abajo porque sino devolveria una region sin tuneles. y lo que habias hecho ants estaba mal porque si te fijas en lo que pide que devuelva la funcion esta pidiendo una Region
    
    en connectedR cambie lo que hiciste porque lo que pide es saber si dos ciudades estan conectadas por un tunel no un link . mas especificamente, use any porque es una función que devuelve un valor booleano y una lista, y devuelve True si al menos un elemento de la lista cumple con  la funcion. acordate que a connectsT hay que pasarle un tunel y dos ciudades, no un link y dos ciudades
    
    ademas de eso no cambie nada

--- Region.hs
+++ Region.hs
@@ -10,22 +10,23 @@ import Tunel
 data Region = Reg [City] [Link] [Tunel]
 
 newR :: Region
-newR = Reg 
+newR = Reg [] [] []
 
 foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
 foundR (Reg cities _ _) newCity | newCity `elem` cities = error "The City is already in the Region" 
-                                | otherwise = Reg (newCity : cities) _ _ 
+                                | otherwise = Reg (newCity:cities) _ _ 
 
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
-linkR (Reg _ links _) c0 c1 qual | 
-                                 | otherwise = Reg _ (newLink : links) _
-                              where newLink = newL c0 c1 quality
-
+linkR (Reg cities links _) c0 c1 quality 
+   | c0 == c1 = error "Cannot link a city to itself"
+   | c0 `notElem` cities || c1 `notElem` cities = error "One or both cities are not in the region"
+   | otherwise = Reg cities ((newL c0 c1 quality):links) tunnels
 
 tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
+tunelR (Reg cities links tunnels) 
 
 connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
-connectedR (Reg _ links _) c0 c1 = connectsT c0 c1 links
+connectedR (Reg _ _ tunnels) c0 c1 = any (connectsT c0 c1) tunnels
 
 linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
 linkedR (Reg _ links _) c0 c1 = foldr (/link acc -> acc || linksL c0 c1 link) False links 
