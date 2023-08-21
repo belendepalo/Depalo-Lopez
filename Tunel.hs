b142759cd11d0629aef1a88f601755b17dae86e1
belendepalo <belen.depalo12@gmail.com>
2023-08-21 14:16:47 -0300

    cambie ortografia

--- Tunel.hs
+++ Tunel.hs
@@ -12,13 +12,13 @@ data Tunel = Tun [Link] deriving (Eq, Show)
 newT :: [Link] -> Tunel
 newT = Tun
 
-connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conceta estas dos ciudades distintas
+connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conecta estas dos ciudades distintas
 connectsT c0 c1 (Tun links) =
    let (Lin cityA _ _) = head links ; (Lin _ cityB _) = last links
    in (cityA == c0 && cityB == c1) || (cityB == c0 && cityA == c1)
 
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
-usesT l_0 (Tun links) = l_0 `elem` links
+usesT lin_0 (Tun links) = lin_0 `elem` links
 
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
 delayT (Tun links) = sum [delayL link | link <- links]
