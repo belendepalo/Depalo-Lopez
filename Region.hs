4de1c44770b36646ea266e5e177268c3dbeadce9
belendepalo <belen.depalo12@gmail.com>
2023-08-21 15:29:33 -0300

    cree una funcion que verifica cordenadas de ciudades - evita repetir codigo

--- Region.hs
+++ Region.hs
@@ -14,13 +14,14 @@ newR = Reg [] [] []
 foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
 foundR (Reg cities _ _) newCity
    | newCity `elem` cities = error "The City is already in the Region"
-   | any (\city -> distanceC city newCity == 0) cities = error "A city with the same coordinates already exists"
+   | any (\city -> sameCoordinates city newCity) cities = error "A city with the same coordinates already exists"
    | otherwise = Reg (newCity:cities) _ _
 
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
 linkR (Reg cities links tunnels) c0 c1 quality
    | c0 == c1 = error "Cannot link a city to itself"
    | not (citiesInRegion c0 c1 cities)  = error "One or both cities are not in the region"
+   | sameCoordinates c0 c1 = error "Both cities cannot have the same coordinates"
    | otherwise = Reg cities (newL c0 c1 quality:links) tunnels
 
 tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
@@ -35,7 +36,7 @@ linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan 
 linkedR (Reg _ links _) c0 c1 = any (\link -> linksL c0 c1 link || linksL c1 c0 link) links
 
 delayR0 :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
-delayR0 (Reg _ links _) c0 c1
+delayR0 (Reg cities links _) c0 c1
    | not (citiesInRegion c0 c1 cities) = error "One or both cities are not in the region"
    | null connectingLinks = error "No direct links between the cities"
    | otherwise = sum (map delayL connectingLinks) / fromIntegral (length connectingLinks)
@@ -43,7 +44,7 @@ delayR0 (Reg _ links _) c0 c1
       connectingLinks = [link | link <- links, linksL c0 c1 link || linksL c1 c0 link]
 
 delayR1 :: Region -> City -> City -> Float
-delayR1 (Reg _ _ tunnels) c0 c1
+delayR1 (Reg cities _ tunnels) c0 c1
    | not (citiesInRegion c0 c1 cities) = error "One or both cities are not in the region"
    | isConnected = delayT connectedTunnel
    | otherwise  = error "The cities are not connected by a tunnel"
@@ -69,4 +70,6 @@ availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad di
 
 
 citiesInRegion :: City -> City -> [City] -> Bool
-citiesInRegion c0 c1 cities = c0 `elem` cities && c1 `elem` cities
+citiesInRegion c0 c1 cities = (c0 `elem` cities) && (c1 `elem` cities)
+
+--capacityUsed 
