279710537c7a1e665d67f6b72757c200459a1193
HowlingPython <126528389+HowlingPython@users.noreply.github.com>
2023-10-28 16:50:16 -0300

    pruebas y primera version del codigo tp4
    esta hecho todo con for loops y conditionals pero funciona y lo podes correr en el cmd haciendo:
     - cd (aca iria la parte que le falta a la direccion)\TP_4\src
    - javac linea/Game.java linea/Linea.java
    -java linea.Game

---
+++ TP_4/src/linea/Linea.java
@@ -0,0 +1,151 @@
+package linea;
+
+public class Linea {
+	private char[][] board;
+	private int width;
+	private int height;
+	private char winVariant;
+
+	public Linea(int width, int height, char winVariant) {
+		this.width = width;
+		this.height = height;
+		this.winVariant = winVariant;
+		this.board = new char[height][width];
+//		este for crea el tablero de espacios vacios
+		for (int rowIndex = 0; rowIndex < height; rowIndex++) {
+			for (int colIndex = 0; colIndex < width; colIndex++) {
+				board[rowIndex][colIndex] = ' ';
+			}
+		}
+	}
+
+	public void playRedAt(int col) {
+		placeChip(col - 1, 'R');
+	}
+
+	public void playBlueAt(int col) {
+		placeChip(col - 1, 'B');
+	}
+
+	private void placeChip(int col, char chip) {
+//		coloca el chip en la columna especificada. si esta llena, lanza una excepcion
+		for (int rowIndex = height - 1; rowIndex >= 0; rowIndex--) {
+			if (board[rowIndex][col] == ' ') {
+				board[rowIndex][col] = chip;
+				return;
+			}
+		}
+		throw new RuntimeException("Column is full!");
+	}
+
+	public String show() {
+		StringBuilder boardStr = new StringBuilder();
+//		loop construye la representacion del tablero en forma de texto.
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				boardStr.append("|").append(board[i][j]);
+			}
+			boardStr.append("|\n");
+		}
+		return boardStr.toString();
+	}	
+	
+	public boolean finished() {
+		return checkWin('R') || checkWin('B') || checkTie();
+	}
+	
+	public String winner() {
+	    if (checkWin('R')) {
+	        return "Red is the winner!";
+	    } else if (checkWin('B')) {
+	        return "Blue is the winner!";
+	    } else if (checkTie()) {
+	        return "The game ended in a tie!";
+	    } else {
+	        return "The game is still ongoing.";
+	    }
+	}
+
+	private boolean checkWin(char chip) {
+//		verifica si el chip especificado gano.
+//		la verificacion depende del valor de la variente de victoria.
+		if (this.winVariant == 'C') {
+			return checkHorizontal(chip) || checkVertical(chip) || checkDiagonal(chip);
+		}
+		else if(this.winVariant == 'A') {
+			return checkHorizontal(chip) || checkVertical(chip);
+		}else {
+			return checkDiagonal(chip); 
+		}
+		
+	}
+
+	private boolean checkHorizontal(char chip) {
+		
+		for (int targetRow = 0; targetRow < height; targetRow++) {
+			int count = 0;
+			for (int targetColumn = 0; targetColumn < width; targetColumn++) {
+				if (board[targetRow][targetColumn] == chip) {
+					count++;
+					if (count == 4)
+						return true;
+				} else {
+					count = 0;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean checkVertical(char chip) {
+		for (int targetColumn = 0; targetColumn < width; targetColumn++) {
+			int count = 0;
+			for (int targetRow = 0; targetRow < height; targetRow++) {
+				if (board[targetRow][targetColumn] == chip) {
+					count++;
+					if (count == 4)
+						return true;
+				} else {
+					count = 0;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean checkDiagonal(char chip) {
+		// Verificación de diagonales principales
+		for (int rowIndex = 0; rowIndex < height - 3; rowIndex++) {
+			for (int colIndex = 0; colIndex < width - 3; colIndex++) {
+				if (board[rowIndex][colIndex] == chip && board[rowIndex + 1][colIndex + 1] == chip && board[rowIndex + 2][colIndex + 2] == chip
+						&& board[rowIndex + 3][colIndex + 3] == chip) {
+					return true;
+				}
+			}
+		}
+
+		// Verificación de diagonales secundarias
+		for (int rowIndex = 3; rowIndex < height; rowIndex++) {
+			for (int colIndex = 0; colIndex < width - 3; colIndex++) {
+				if (board[rowIndex][colIndex] == chip && board[rowIndex - 1][colIndex + 1] == chip && board[rowIndex - 2][colIndex + 2] == chip
+						&& board[rowIndex - 3][colIndex + 3] == chip) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	private boolean checkTie() {
+//		si la parte superior de todas las columnas esta ocupada, es empate.
+		for (int colIndex = 0; colIndex < width; colIndex++) {
+			if (board[0][colIndex] == ' ') {
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+}
