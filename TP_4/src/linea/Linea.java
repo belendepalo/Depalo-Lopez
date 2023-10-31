21186e222a881e8dcc9cbc582ef200381c61bae8
belendepalo <104471058+belendepalo@users.noreply.github.com>
2023-10-31 21:51:33 +0000

    Update Linea.java
    Turnos y orden de cada uno

--- TP_4/src/linea/Linea.java
+++ TP_4/src/linea/Linea.java
@@ -1,133 +1,187 @@
 package linea;
 
-import java.util.stream.IntStream;
-
 public class Linea {
 	private char[][] board;
 	private int width;
 	private int height;
 	private char winVariant;
+	private char turn;
 
 	public Linea(int width, int height, char winVariant) {
 		this.width = width;
 		this.height = height;
 		this.winVariant = winVariant;
 		this.board = new char[height][width];
+		this.turn = 'R';
 		
-//		El stream llena el tablero de espacios vacíos.
-		IntStream.range(0, height).forEach(rowIndex -> IntStream.range(0, width).forEach(colIndex -> board[rowIndex][colIndex] = ' '));
-
+	
+		
+//		este for crea el tablero de espacios vacios
+		for (int rowIndex = 0; rowIndex < height; rowIndex++) {
+			for (int colIndex = 0; colIndex < width; colIndex++) {
+				board[rowIndex][colIndex] = ' ';
+			}
+		}
+	}
+	
+	
+	public void setTurn(char turn) {
+		this.turn = turn;
 	}
 
+	public boolean isRedsTurn() {
+		return getTurn() == 'R';
+	}
+	public boolean isBluesTurn() {
+		return getTurn() == 'B';
+	}
+	
+
 	public void playRedAt(int col) {
-		placeChip(col - 1, 'R');
+		if (turn == 'R') {
+			placeChip(col - 1, 'R');
+            turn = 'B';
+        } else {
+            throw new RuntimeException("It's Blue's Turn!");
+        }
+
 	}
 
 	public void playBlueAt(int col) {
-		placeChip(col - 1, 'B');
+		if (turn == 'B') {
+			placeChip(col - 1, 'B');
+            turn = 'R';
+        } else {
+            throw new RuntimeException("It's Red's Turn!");
+        }
+		
+	}
+
+	private void placeChip(int col, char chip) {
+//		coloca el chip en la columna especificada. si esta llena, lanza una excepcion
+		for (int rowIndex = height - 1; rowIndex >= 0; rowIndex--) {
+			if (board[rowIndex][col] == ' ') {
+				board[rowIndex][col] = chip;
+				return;
+			}
+		}
+		throw new RuntimeException("Column is full!");
 	}
 
 	public String show() {
 		StringBuilder boardStr = new StringBuilder();
-//		El stream construye la representación del tablero en forma de texto.
-		IntStream.range(0, height).forEach(rowIndex -> {
-			IntStream.range(0, width).forEach(colIndex -> boardStr.append("|").append(board[rowIndex][colIndex]));
+//		loop construye la representacion del tablero en forma de texto.
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				boardStr.append("|").append(board[i][j]);
+			}
 			boardStr.append("|\n");
-		});
+		}
 		return boardStr.toString();
-	}
-
+	}	
+	
 	public boolean finished() {
-//		Si alguno gano o termino en empate, devuelve true.
 		return checkWin('R') || checkWin('B') || checkTie();
 	}
-
+	
 	public String winner() {
-//		Cheque quien gana y devuelvo el mensaje adecuado.
-		if (checkWin('R')) {
-			return "Red is the winner!";
-		} else if (checkWin('B')) {
-			return "Blue is the winner!";
-		} else if (checkTie()) {
-			return "The game ended in a tie!";
-		} else {
-			return "";
-		}
-	}
-
-	private void placeChip(int col, char chip) {		
-//		Coloca el chip en la columna especificada. Si está llena lanza una excepción.
-		int validRowIndex = IntStream.iterate(height - 1, rowIndex -> rowIndex - 1)
-				.filter(rowIndex -> board[rowIndex][col] == ' ')
-				.findFirst().orElse(-1);
-
-		if (validRowIndex != -1) {
-			board[validRowIndex][col] = chip;
-		} else {
-			throw new RuntimeException("Column is full!");
-		}
+	    if (checkWin('R')) {
+	        return "Red is the winner!";
+	    } else if (checkWin('B')) {
+	        return "Blue is the winner!";
+	    } else if (checkTie()) {
+	        return "The game ended in a tie!";
+	    } else {
+	        return "The game is still ongoing.";
+	    }
 	}
 
 	private boolean checkWin(char chip) {
-//		Verifica si el chip especificado ganó.
-//		La verificación depende del valor de la variante de victoria.
-		
-		if (this.winVariant == 'A') {
-			return checkHorizontalWin(chip) || checkVerticalWin(chip);
-		} else if (this.winVariant == 'B') {
-			return checkDiagonalWin(chip);
+//		verifica si el chip especificado gano.
+//		la verificacion depende del valor de la variente de victoria.
+		if (this.winVariant == 'C') {
+			return checkHorizontal(chip) || checkVertical(chip) || checkDiagonal(chip);
 		}
-		return checkHorizontalWin(chip) || checkVerticalWin(chip) || checkDiagonalWin(chip);
+		else if(this.winVariant == 'A') {
+			return checkHorizontal(chip) || checkVertical(chip);
+		}else {
+			return checkDiagonal(chip); 
+		}
+		
+	}
 
+	private boolean checkHorizontal(char chip) {
+		
+		for (int targetRow = 0; targetRow < height; targetRow++) {
+			int count = 0;
+			for (int targetColumn = 0; targetColumn < width; targetColumn++) {
+				if (board[targetRow][targetColumn] == chip) {
+					count++;
+					if (count == 4)
+						return true;
+				} else {
+					count = 0;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean checkVertical(char chip) {
+		for (int targetColumn = 0; targetColumn < width; targetColumn++) {
+			int count = 0;
+			for (int targetRow = 0; targetRow < height; targetRow++) {
+				if (board[targetRow][targetColumn] == chip) {
+					count++;
+					if (count == 4)
+						return true;
+				} else {
+					count = 0;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean checkDiagonal(char chip) {
+		// Verificación de diagonales principales
+		for (int rowIndex = 0; rowIndex < height - 3; rowIndex++) {
+			for (int colIndex = 0; colIndex < width - 3; colIndex++) {
+				if (board[rowIndex][colIndex] == chip && board[rowIndex + 1][colIndex + 1] == chip && board[rowIndex + 2][colIndex + 2] == chip
+						&& board[rowIndex + 3][colIndex + 3] == chip) {
+					return true;
+				}
+			}
+		}
+
+		// Verificación de diagonales secundarias
+		for (int rowIndex = 3; rowIndex < height; rowIndex++) {
+			for (int colIndex = 0; colIndex < width - 3; colIndex++) {
+				if (board[rowIndex][colIndex] == chip && board[rowIndex - 1][colIndex + 1] == chip && board[rowIndex - 2][colIndex + 2] == chip
+						&& board[rowIndex - 3][colIndex + 3] == chip) {
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 
 	private boolean checkTie() {
-//		Si la parte superior de todas las columnas está ocupada, es empate.
-		return IntStream.range(0, width).allMatch(colIndex -> board[0][colIndex] != ' ');
-	}
-
-	private boolean checkHorizontalWin(char chip) {
-		return IntStream.range(0, height).anyMatch(targetRow -> hasConsecutiveChips(chip, 4,
-				IntStream.range(0, width).map(targetColumn -> board[targetRow][targetColumn])));
-	}
-
-	private boolean checkVerticalWin(char chip) {
-		return IntStream.range(0, width).anyMatch(targetColumn -> hasConsecutiveChips(chip, 4,
-				IntStream.range(0, height).map(targetRow -> board[targetRow][targetColumn])));
+//		si la parte superior de todas las columnas esta ocupada, es empate.
+		for (int colIndex = 0; colIndex < width; colIndex++) {
+			if (board[0][colIndex] == ' ') {
+				return false;
+			}
+		}
+		return true;
 	}
 	
-	private boolean checkDiagonalWin(char chip) {
-	    return IntStream.range(0, height - 3)
-	            .anyMatch(rowIndex -> IntStream.range(0, width - 3)
-	                    .anyMatch(colIndex -> checkDiagonalSequence(rowIndex, colIndex, 1, 1, chip)))
-	           ||
-	           IntStream.range(3, height)
-	            .anyMatch(rowIndex -> IntStream.range(0, width - 3)
-	                    .anyMatch(colIndex -> checkDiagonalSequence(rowIndex, colIndex, -1, 1, chip)));
-	}
 	
-	private boolean hasConsecutiveChips(char chip, int requiredConsecutiveChips, IntStream chipSequence) {
-		/**
-		 * Verifica si una secuencia proporcionada contiene una cantidad requerida de
-		 * chips consecutivos.
-		 */
-		int[] sequenceArray = chipSequence.toArray();
-
-		return IntStream.range(0, sequenceArray.length - requiredConsecutiveChips + 1)
-				.anyMatch(startIndex -> IntStream.range(startIndex, startIndex + requiredConsecutiveChips)
-						.allMatch(sequenceIndex -> sequenceArray[sequenceIndex] == chip));
-	}
-
-	private boolean checkDiagonalSequence(int rowStart, int colStart, int rowIncrement, int colIncrement, char chip) {
-
-//		 Verifica una secuencia diagonal en una dirección dada.
-
-	    return IntStream.range(0, 4)
-	            .allMatch(step -> {
-	                int targetRowIndex = rowStart + step * rowIncrement;
-	                int targetColumnIndex = colStart + step * colIncrement;
-	                return targetRowIndex >= 0 && targetRowIndex < height && targetColumnIndex >= 0 && targetColumnIndex < width && board[targetRowIndex][targetColumnIndex] == chip;
-	            });
+	
+	// Extras
+	public char getTurn() {
+		return turn;
 	}
 
 }
