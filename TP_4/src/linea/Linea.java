2e2481ce371a14cc455be1226567db159eee1c1d
belendepalo <104471058+belendepalo@users.noreply.github.com>
2023-11-02 15:02:25 +0000

    Update Linea.java
    Cambio de char [] [] a ArrayList

--- TP_4/src/linea/Linea.java
+++ TP_4/src/linea/Linea.java
@@ -1,187 +1,176 @@
 package linea;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Linea {
-	private char[][] board;
-	private int width;
-	private int height;
-	private char winVariant;
-	private char turn;
+    private static final String ErrorItsRedsTurn = "It's Red's Turn!";
+    private static final String ErrorItsBluesTurn = "It's Blue's Turn!";
+    private List<List<Character>> board;
+    private int width;
+    private int height;
+    private char winVariant;
+    private char turn;
 
-	public Linea(int width, int height, char winVariant) {
-		this.width = width;
-		this.height = height;
-		this.winVariant = winVariant;
-		this.board = new char[height][width];
-		this.turn = 'R';
-		
-	
-		
-//		este for crea el tablero de espacios vacios
-		for (int rowIndex = 0; rowIndex < height; rowIndex++) {
-			for (int colIndex = 0; colIndex < width; colIndex++) {
-				board[rowIndex][colIndex] = ' ';
-			}
-		}
-	}
-	
-	
-	public void setTurn(char turn) {
-		this.turn = turn;
-	}
+    public Linea(int width, int height, char winVariant) {
+        this.width = width;
+        this.height = height;
+        this.winVariant = winVariant;
+        this.board = new ArrayList<>();
+        for (int rowIndex = 0; rowIndex < height; rowIndex++) {
+            List<Character> row = new ArrayList<>();
+            for (int colIndex = 0; colIndex < width; colIndex++) {
+                row.add(' ');
+            }
+            this.board.add(row);
+        }
+        this.turn = 'R';
+    }
 
-	public boolean isRedsTurn() {
-		return getTurn() == 'R';
-	}
-	public boolean isBluesTurn() {
-		return getTurn() == 'B';
-	}
-	
+    public void setTurn(char turn) {
+        this.turn = turn;
+    }
 
-	public void playRedAt(int col) {
-		if (turn == 'R') {
-			placeChip(col - 1, 'R');
+    public boolean isRedsTurn() {
+        return getTurn() == 'R';
+    }
+
+    public boolean isBluesTurn() {
+        return getTurn() == 'B';
+    }
+
+    public void playRedAt(int col) {
+        if (turn == 'R') {
+            placeChip(col - 1, 'R');
             turn = 'B';
         } else {
-            throw new RuntimeException("It's Blue's Turn!");
+            throw new RuntimeException(ErrorItsBluesTurn);
         }
+    }
 
-	}
-
-	public void playBlueAt(int col) {
-		if (turn == 'B') {
-			placeChip(col - 1, 'B');
+    public void playBlueAt(int col) {
+        if (turn == 'B') {
+            placeChip(col - 1, 'B');
             turn = 'R';
         } else {
-            throw new RuntimeException("It's Red's Turn!");
+            throw new RuntimeException(ErrorItsRedsTurn);
         }
-		
-	}
+    }
 
-	private void placeChip(int col, char chip) {
-//		coloca el chip en la columna especificada. si esta llena, lanza una excepcion
-		for (int rowIndex = height - 1; rowIndex >= 0; rowIndex--) {
-			if (board[rowIndex][col] == ' ') {
-				board[rowIndex][col] = chip;
-				return;
-			}
-		}
-		throw new RuntimeException("Column is full!");
-	}
+    public void placeChip(int col, char chip) {
+        for (int rowIndex = height - 1; rowIndex >= 0; rowIndex--) {
+            if (board.get(rowIndex).get(col) == ' ') {
+                board.get(rowIndex).set(col, chip);
+                return;
+            }
+        }
+        throw new RuntimeException("Column is full!");
+    }
 
-	public String show() {
-		StringBuilder boardStr = new StringBuilder();
-//		loop construye la representacion del tablero en forma de texto.
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				boardStr.append("|").append(board[i][j]);
-			}
-			boardStr.append("|\n");
-		}
-		return boardStr.toString();
-	}	
-	
-	public boolean finished() {
-		return checkWin('R') || checkWin('B') || checkTie();
-	}
-	
-	public String winner() {
-	    if (checkWin('R')) {
-	        return "Red is the winner!";
-	    } else if (checkWin('B')) {
-	        return "Blue is the winner!";
-	    } else if (checkTie()) {
-	        return "The game ended in a tie!";
-	    } else {
-	        return "The game is still ongoing.";
-	    }
-	}
+    public String show() {
+        StringBuilder boardStr = new StringBuilder();
+        for (List<Character> row : board) {
+            for (char cell : row) {
+                boardStr.append("|").append(cell);
+            }
+            boardStr.append("|\n");
+        }
+        return boardStr.toString();
+    }
 
-	private boolean checkWin(char chip) {
-//		verifica si el chip especificado gano.
-//		la verificacion depende del valor de la variente de victoria.
-		if (this.winVariant == 'C') {
-			return checkHorizontal(chip) || checkVertical(chip) || checkDiagonal(chip);
-		}
-		else if(this.winVariant == 'A') {
-			return checkHorizontal(chip) || checkVertical(chip);
-		}else {
-			return checkDiagonal(chip); 
-		}
-		
-	}
+    public boolean finished() {
+        return checkWin('R') || checkWin('B') || checkTie();
+    }
 
-	private boolean checkHorizontal(char chip) {
-		
-		for (int targetRow = 0; targetRow < height; targetRow++) {
-			int count = 0;
-			for (int targetColumn = 0; targetColumn < width; targetColumn++) {
-				if (board[targetRow][targetColumn] == chip) {
-					count++;
-					if (count == 4)
-						return true;
-				} else {
-					count = 0;
-				}
-			}
-		}
-		return false;
-	}
+    public String winner() {
+        if (checkWin('R')) {
+            return "Red is the winner!";
+        } else if (checkWin('B')) {
+            return "Blue is the winner!";
+        } else if (checkTie()) {
+            return "The game ended in a tie!";
+        } else {
+            return "The game is still ongoing.";
+        }
+    }
 
-	private boolean checkVertical(char chip) {
-		for (int targetColumn = 0; targetColumn < width; targetColumn++) {
-			int count = 0;
-			for (int targetRow = 0; targetRow < height; targetRow++) {
-				if (board[targetRow][targetColumn] == chip) {
-					count++;
-					if (count == 4)
-						return true;
-				} else {
-					count = 0;
-				}
-			}
-		}
-		return false;
-	}
+    private boolean checkWin(char chip) {
+        if (this.winVariant == 'C') {
+            return checkHorizontal(chip) || checkVertical(chip) || checkDiagonal(chip);
+        } else if (this.winVariant == 'A') {
+            return checkHorizontal(chip) || checkVertical(chip);
+        } else {
+            return checkDiagonal(chip);
+        }
+    }
 
-	private boolean checkDiagonal(char chip) {
-		// Verificación de diagonales principales
-		for (int rowIndex = 0; rowIndex < height - 3; rowIndex++) {
-			for (int colIndex = 0; colIndex < width - 3; colIndex++) {
-				if (board[rowIndex][colIndex] == chip && board[rowIndex + 1][colIndex + 1] == chip && board[rowIndex + 2][colIndex + 2] == chip
-						&& board[rowIndex + 3][colIndex + 3] == chip) {
-					return true;
-				}
-			}
-		}
+    private boolean checkHorizontal(char chip) {
+        for (int rowIndex = 0; rowIndex < height; rowIndex++) {
+            int count = 0;
+            for (int colIndex = 0; colIndex < width; colIndex++) {
+                if (board.get(rowIndex).get(colIndex) == chip) {
+                    count++;
+                    if (count == 4)
+                        return true;
+                } else {
+                    count = 0;
+                }
+            }
+        }
+        return false;
+    }
 
-		// Verificación de diagonales secundarias
-		for (int rowIndex = 3; rowIndex < height; rowIndex++) {
-			for (int colIndex = 0; colIndex < width - 3; colIndex++) {
-				if (board[rowIndex][colIndex] == chip && board[rowIndex - 1][colIndex + 1] == chip && board[rowIndex - 2][colIndex + 2] == chip
-						&& board[rowIndex - 3][colIndex + 3] == chip) {
-					return true;
-				}
-			}
-		}
+    private boolean checkVertical(char chip) {
+        for (int colIndex = 0; colIndex < width; colIndex++) {
+            int count = 0;
+            for (int rowIndex = 0; rowIndex < height; rowIndex++) {
+                if (board.get(rowIndex).get(colIndex) == chip) {
+                    count++;
+                    if (count == 4)
+                        return true;
+                } else {
+                    count = 0;
+                }
+            }
+        }
+        return false;
+    }
 
-		return false;
-	}
+    private boolean checkDiagonal(char chip) {
+        // Verificación de diagonales principales
+        for (int rowIndex = 0; rowIndex < height - 3; rowIndex++) {
+            for (int colIndex = 0; colIndex < width - 3; colIndex++) {
+                if (board.get(rowIndex).get(colIndex) == chip && board.get(rowIndex + 1).get(colIndex + 1) == chip
+                        && board.get(rowIndex + 2).get(colIndex + 2) == chip && board.get(rowIndex + 3).get(colIndex + 3) == chip) {
+                    return true;
+                }
+            }
+        }
 
-	private boolean checkTie() {
-//		si la parte superior de todas las columnas esta ocupada, es empate.
-		for (int colIndex = 0; colIndex < width; colIndex++) {
-			if (board[0][colIndex] == ' ') {
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	
-	
-	// Extras
-	public char getTurn() {
-		return turn;
-	}
+        // Verificación de diagonales secundarias
+        for (int rowIndex = 3; rowIndex < height; rowIndex++) {
+            for (int colIndex = 0; colIndex < width - 3; colIndex++) {
+                if (board.get(rowIndex).get(colIndex) == chip && board.get(rowIndex - 1).get(colIndex + 1) == chip
+                        && board.get(rowIndex - 2).get(colIndex + 2) == chip && board.get(rowIndex - 3).get(colIndex + 3) == chip) {
+                    return true;
+                }
+            }
+        }
 
+        return false;
+    }
+
+    private boolean checkTie() {
+        for (char cell : board.get(0)) {
+            if (cell == ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Extras
+    public char getTurn() {
+        return turn;
+    }
 }
