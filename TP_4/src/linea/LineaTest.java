3ae0920c27755b568691c056b7a9ba7f399aaa30
HowlingPython <126528389+HowlingPython@users.noreply.github.com>
2023-11-04 15:32:34 -0300

    Mejore los tests con assertThrow y @BeforeEach

--- TP_4/src/linea/LineaTest.java
+++ TP_4/src/linea/LineaTest.java
@@ -1,25 +1,29 @@
 package linea;
 
 import static org.junit.Assert.assertEquals;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LineaTest {
+
+    private Linea game;
+
+    @BeforeEach
+    public void setUp() {
+        game = new Linea(7, 6, 'C');
+    }
 	@Test
 	public void test00_RedAlwaysStarts() {
-		Linea game = new Linea(7, 6, 'C');
 		assertTrue(game.isRedsTurn());
 		assertFalse(game.isBluesTurn());
 	}
 
 	@Test
 	public void test01_AfterRedPlaysIsBlueTurn() {
-		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(1);
 		assertFalse(game.isRedsTurn());
 		assertTrue(game.isBluesTurn());
@@ -28,34 +32,21 @@ public class LineaTest {
 
 	@Test
 	public void test02_ErrorIsThrownAfterRedTriesToPlayTwice() {
-		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(1);
-		try {
-			game.playRedAt(3);
-			fail("Se esperaba una excepción, pero no se lanzó.");
-		} catch (RuntimeException e) {
-			assertEquals("It's Blue's Turn!", e.getMessage());
-		}
-
+		Exception exception = assertThrows(RuntimeException.class, () -> game.playRedAt(3));
+		assertEquals("It's Blue's Turn!", exception.getMessage());
 	}
 
 	@Test
 	public void test03_ErrorIsThrownAfterBlueTriesToPlayTwice() {
-		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(1);
 		game.playBlueAt(2);
-		try {
-			game.playBlueAt(3);
-			fail("Se esperaba una excepción, pero no se lanzó.");
-		} catch (RuntimeException e) {
-			assertEquals("It's Red's Turn!", e.getMessage());
-		}
-
+		Exception exception = assertThrows(RuntimeException.class, () -> game.playBlueAt(3));
+		assertEquals("It's Red's Turn!", exception.getMessage());
 	}
 
 	@Test
 	public void test04_RedsPlayIsValid() {
-		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(1);
 		assertTrue(game.show().contains("R"));
 
@@ -63,7 +54,6 @@ public class LineaTest {
 
 	@Test
 	public void test05_BluesPlayIsValid() {
-		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(1);
 		game.playBlueAt(2);
 		assertTrue(game.show().contains("B"));
@@ -72,31 +62,18 @@ public class LineaTest {
 
 	@Test
 	public void test06_RedsTriesToPlayInAColumnThatIsNotInTheGame() {
-		Linea game = new Linea(7, 6, 'C');
-		try {
-			game.playRedAt(0);
-			fail("Se esperaba una excepción, pero no se lanzó.");
-		} catch (RuntimeException e) {
-			assertEquals("Column out of bounds!", e.getMessage());
-		}
-
+		Exception exception = assertThrows(RuntimeException.class, () -> game.playRedAt(0));
+		assertEquals("Column out of bounds!", exception.getMessage());
 	}
 
 	@Test
 	public void test07_BluesTriesToPlayInAColumnThatIsNotInTheGame() {
-		Linea game = new Linea(7, 6, 'C');
-		try {
-			game.playRedAt(0);
-			fail("Se esperaba una excepción, pero no se lanzó.");
-		} catch (RuntimeException e) {
-			assertEquals("Column out of bounds!", e.getMessage());
-		}
-
+		Exception exception = assertThrows(RuntimeException.class, () -> game.playBlueAt(0));
+		assertEquals("Column out of bounds!", exception.getMessage());
 	}
 
 	@Test
 	public void test08_RedsAndBluesPlaysAreValidAndTheGameIsStillGoing() {
-		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(1);
 		game.playBlueAt(2);
 		assertTrue(game.show().contains("R"));
@@ -105,25 +82,18 @@ public class LineaTest {
 
 	}
 
-	@Test
-	public void test09_ErrorIsThrownAfterRedsTryToPlayWhenColumnIsFull() {
-		Linea game = new Linea(7, 6, 'C');
-		for (int i = 0; i < 3; i++) {
-			game.playRedAt(3);
-			game.playBlueAt(3);
-		}
-
-		try {
-			game.playRedAt(3);
-			fail("Se esperaba una excepción, pero no se lanzó.");
-		} catch (RuntimeException e) {
-			assertEquals("Column is full!", e.getMessage());
-		}
-	}
+    @Test
+    public void test09_ErrorIsThrownAfterRedsTryToPlayWhenColumnIsFull() {
+        for (int i = 0; i < 3; i++) {
+            game.playRedAt(3);
+            game.playBlueAt(3);
+        }
+        Exception exception = assertThrows(RuntimeException.class, () -> game.playRedAt(3));
+        assertEquals("Column is full!", exception.getMessage());
+    }
 
 	@Test
 	public void test10_RedWinsHorizontally() {
-		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(1);
 		game.playBlueAt(1);
 		game.playRedAt(2);
@@ -138,7 +108,6 @@ public class LineaTest {
 
 	@Test
 	public void test11_BlueWinsVertically() {
-		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(2);
 		game.playBlueAt(1);
 		game.playRedAt(3);
@@ -154,7 +123,6 @@ public class LineaTest {
 
 	@Test
 	public void test12_RedWinsInDiagonal() {
-		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(1);
 		game.playBlueAt(2);
 		game.playRedAt(2);
@@ -173,7 +141,6 @@ public class LineaTest {
 
 	@Test
 	public void test13_GameEndedInATie() {
-		Linea game = new Linea(3, 3, 'C');
 		game.playRedAt(1);
 		game.playBlueAt(1);
 		game.playRedAt(1);
