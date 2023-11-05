3cb9abf648e0d4d8cf33e96921ed308cdb7e782a
belendepalo <104471058+belendepalo@users.noreply.github.com>
2023-11-05 00:57:57 -0300

    Update BluesTurn.java
    Polimorfismo de turnos

--- TP_4/src/linea/BluesTurn.java
+++ TP_4/src/linea/BluesTurn.java
@@ -1,17 +1,26 @@
 package linea;
+import java.util.List;
 
-public class BluesTurn extends GameStatus {
+public class BluesTurn extends GameStatus{
+    @Override
+    public void playBlueAt(int col, List<List<Character>> board) {
+        // Verificar si la columna está llena
+        if (board.get(0).get(col - 1) != ' ') {
+            throw new RuntimeException("Column is full!");
+        }
+
+        // Colocar la ficha 'B' en la posición correspondiente
+        for (int rowIndex = board.size() - 1; rowIndex >= 0; rowIndex--) {
+            if (board.get(rowIndex).get(col - 1) == ' ') {
+                board.get(rowIndex).set(col - 1, 'B');
+                return;
+            }
+        }
+    }
+        
 	@Override
-	public void playBlueAt(int col) {
-		// grid.placeChip(col - 1, 'B'); Se coloca la ficha
-		// turn = 'R'; Se cambia de turno, ahora van las rojas
-
-	}
-
-	@Override
-	public void playRedAt(int col) {
-
-		throw new RuntimeException(ErrorItsBluesTurn);
-	}
+	public void playRedAt(int col, List<List<Character>> board) {
+        throw new RuntimeException(ErrorItsBluesTurn);
+    }
 
 }
