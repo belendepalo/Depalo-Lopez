9bdbcd58d3ca16f963c5a43406a4b78515877887
belendepalo <104471058+belendepalo@users.noreply.github.com>
2023-11-06 11:33:59 -0300

    Update GameStatus.java

--- TP_4/src/linea/GameStatus.java
+++ TP_4/src/linea/GameStatus.java
@@ -1,21 +1,39 @@
 package linea;
 
+import java.util.Arrays;
 import java.util.List;
 
 public abstract class GameStatus {
-
-	protected static final String ERROR_GAME_OVER = "Game Over!";
-	protected static final char RED_CHIP = 'R';
-	protected static final char BLUE_CHIP = 'B';
-
-	public abstract String show(List<List<Character>> board);
-
-	public abstract boolean finished(List<List<Character>> board);
-
-	public abstract void playRedAt(int col, List<List<Character>> board);
-
-	public abstract void playBlueAt(int col, List<List<Character>> board);
 	
-    public abstract GameStatus checkAndUpdateGameState(List<List<Character>> board);
-
+	private static char chipB = 'B';
+	private static char chipR = 'R';
+	protected static String ErrorItsRedsTurn = "It's Red's Turn!";
+	protected static String ErrorItsBluesTurn = "It's Blue's Turn!";
+	private List<WinningVariants> winningVariantsList = Arrays.asList(new WinningVariantA(), new WinningVariantB(), new WinningVariantC());
+	public WinningVariants winVariants;
+	
+	public abstract void playBlueAt(int col, List<List<Character>> board);
+    public abstract void playRedAt(int col, List<List<Character>> board);
+	public abstract boolean isBluesTurn();
+	public abstract boolean isRedsTurn();
+	public abstract GameStatus checkAndUpdateGameState(List<List<Character>> board);
+	
+    public boolean checkWin(char chip, List<List<Character>> board) {
+        return winVariants.checkWin(chip, board);
+    }
+	
+	public boolean checkTie(List<List<Character>> board) {
+		return board.get(0).stream().allMatch(cell -> cell != ' ');
+    }
+	
+	public boolean finished(List<List<Character>> board) {
+		return checkWin(chipR, board) || checkWin(chipB, board) || checkTie(board);
+	}
+	
+	protected void initializeWinningVariant(char winVariant) {
+    	winningVariantsList.stream().filter(variant -> variant
+    	.canHandle(winVariant)).findFirst()
+        .ifPresent(variant -> winVariants = variant);
+    }
+	
 }
