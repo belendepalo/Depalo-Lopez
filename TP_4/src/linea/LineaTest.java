6d360f9eb34df17434a67fff951e97cb2dc2c00b
belendepalo <104471058+belendepalo@users.noreply.github.com>
2023-11-06 11:34:45 -0300

    Update LineaTest.java

--- TP_4/src/linea/LineaTest.java
+++ TP_4/src/linea/LineaTest.java
@@ -2,6 +2,7 @@ package linea;
 
 import static org.junit.Assert.assertEquals;
 
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -11,118 +12,168 @@ import org.junit.jupiter.api.Test;
 
 public class LineaTest {
 	@Test
-	public void test00_RedAlwaysStarts() {
+	public void test00_RedAlwaysStarts(){
 		Linea game = new Linea(7, 6, 'C');
-		assertTrue(game.gameStatus instanceof RedsTurn);
-		assertFalse(game.gameStatus instanceof BluesTurn);
+	    assertTrue(game.isRedsTurn());
+	    assertFalse(game.isBluesTurn());
 	}
-
+	
 	@Test
-	public void test01_AfterRedPlaysIsBlueTurn() {
+	public void test01_AfterRedPlaysIsBlueTurn(){
 		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(1);
-		assertFalse(game.gameStatus instanceof RedsTurn);
-		assertTrue(game.gameStatus instanceof BluesTurn);
-
+		assertFalse(game.isRedsTurn());
+		assertTrue(game.isBluesTurn());
+	    
 	}
-
+	
 	@Test
-	public void test02_ErrorIsThrownAfterRedTriesToPlayTwice() {
+	public void test02_ErrorIsThrownAfterRedTriesToPlayTwice(){
 		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(1);
 		try {
-			game.playRedAt(3);
-			fail("Se esperaba una excepción, pero no se lanzó.");
-		} catch (RuntimeException e) {
-			assertEquals("It's Blue's Turn!", e.getMessage());
-		}
-
+	        game.playRedAt(3);
+	        fail("Se esperaba una excepción, pero no se lanzó.");
+	    } catch (RuntimeException e) {
+	        assertEquals("It's Blue's Turn!", e.getMessage());
+	    }
+	    
 	}
-
+	
 	@Test
-	public void test03_ErrorIsThrownAfterBlueTriesToPlayTwice() {
+	public void test03_ErrorIsThrownAfterBlueTriesToPlayTwice(){
 		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(1);
 		game.playBlueAt(2);
 		try {
-			game.playBlueAt(3);
-			fail("Se esperaba una excepción, pero no se lanzó.");
-		} catch (RuntimeException e) {
-			assertEquals("It's Red's Turn!", e.getMessage());
-		}
-
+	        game.playBlueAt(3);
+	        fail("Se esperaba una excepción, pero no se lanzó.");
+	    } catch (RuntimeException e) {
+	        assertEquals("It's Red's Turn!", e.getMessage());
+	    }
+	    
 	}
-
+	
 	@Test
-	public void test04_RedsPlayIsValid() {
+	public void test04_RedsPlayIsValid(){
 		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(1);
-		assertTrue(game.show().contains("R"));
-
+	    assertTrue(game.show().contains("R"));
+	    
 	}
-
+	
 	@Test
-	public void test05_BluesPlayIsValid() {
-		Linea game = new Linea(7, 6, 'C');
-		game.playRedAt(1);
-		game.playBlueAt(2);
-		assertTrue(game.show().contains("B"));
-
-	}
-
-	@Test
-	public void test06_RedsTriesToPlayInAColumnThatIsNotInTheGame() {
-		Linea game = new Linea(7, 6, 'C');
-		try {
-			game.playRedAt(0);
-			fail("Se esperaba una excepción, pero no se lanzó.");
-		} catch (RuntimeException e) {
-			assertEquals("Column out of bounds!", e.getMessage());
-		}
-
-	}
-
-	@Test
-	public void test07_BluesTriesToPlayInAColumnThatIsNotInTheGame() {
-		Linea game = new Linea(7, 6, 'C');
-		try {
-			game.playRedAt(0);
-			fail("Se esperaba una excepción, pero no se lanzó.");
-		} catch (RuntimeException e) {
-			assertEquals("Column out of bounds!", e.getMessage());
-		}
-
-	}
-
-	@Test
-	public void test08_RedsAndBluesPlaysAreValidAndTheGameIsStillGoing() {
+	public void test05_BluesPlayIsValid(){
 		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(1);
 		game.playBlueAt(2);
-		assertTrue(game.show().contains("R"));
-		assertTrue(game.show().contains("B"));
-//		assertEquals("The game is still ongoing.", game.winner());
-
+	    assertTrue(game.show().contains("B"));
+	    
 	}
-
+	
 	@Test
-	public void test09_ErrorIsThrownAfterRedsTryToPlayWhenColumnIsFull() {
+	public void test06_RedsTriesToPlayInAColumnThatIsNotInTheGame(){
 		Linea game = new Linea(7, 6, 'C');
-		for (int i = 0; i < 3; i++) {
-			game.playRedAt(3);
-			game.playBlueAt(3);
-		}
-
 		try {
-			game.playRedAt(3);
-			fail("Se esperaba una excepción, pero no se lanzó.");
-		} catch (RuntimeException e) {
-			assertEquals("Column is full!", e.getMessage());
-		}
+	        game.playRedAt(0);
+	        fail("Se esperaba una excepción, pero no se lanzó.");
+	    } catch (RuntimeException e) {
+	        assertEquals(Linea.ErrorColumnOutOfBounds, e.getMessage());
+	    }
+	    
 	}
-
+	
 	@Test
-	public void test10_RedWinsHorizontally() {
+	public void test07_BluesTriesToPlayInAColumnThatIsNotInTheGame(){
+		Linea game = new Linea(7, 6, 'C');
+		try {
+	        game.playBlueAt(0);
+	        fail("Se esperaba una excepción, pero no se lanzó.");
+	    } catch (RuntimeException e) {
+	        assertEquals(Linea.ErrorColumnOutOfBounds, e.getMessage());
+	    }
+	    
+	}
+	
+	@Test
+	public void test08_RedsAndBluesPlaysAreValidAndTheGameIsStillGoing(){
+		Linea game = new Linea(7, 6, 'C');
+		game.playRedAt(1);
+		game.playBlueAt(2);
+	    assertTrue(game.show().contains("R"));
+	    assertTrue(game.show().contains("B"));
+	    assertEquals("The game is still ongoing.", game.winner());
+	    
+	    
+	}
+	
+	@Test
+	public void test09_ErrorIsThrownAfterRedsTryToPlayWhenColumnIsFull(){
+		Linea game = new Linea(7, 6, 'C');
+	    for (int i = 0; i < 3; i++) {
+	        game.playRedAt(3);
+	        game.playBlueAt(3);
+	    }
+
+	    try {
+	        game.playRedAt(3);
+	        fail("Se esperaba una excepción, pero no se lanzó.");
+	    } catch (RuntimeException e) {
+	        assertEquals("Column is full!", e.getMessage());
+	    }
+	}
+	
+	@Test
+	public void test10_RedWinsHorizontallyWithWinningVariantA(){
+		Linea game = new Linea(7, 6, 'A');
+		game.playRedAt(1);
+		game.playBlueAt(1);
+		game.playRedAt(2);
+		game.playBlueAt(1);
+		game.playRedAt(3);
+		game.playBlueAt(1);
+		game.playRedAt(4);
+		assertTrue(game.finished());
+		assertEquals("Red is the winner!", game.winner());
+		    
+	}
+	
+	@Test
+	public void test11_BlueWinsVerticallyWithWinningVariantA(){
+		Linea game = new Linea(7, 6, 'A');
+		game.playRedAt(2);
+		game.playBlueAt(1);
+		game.playRedAt(3);
+		game.playBlueAt(1);
+		game.playRedAt(4);
+		game.playBlueAt(1);
+		game.playRedAt(2);
+		game.playBlueAt(1);
+		assertTrue(game.finished());
+		assertEquals("Blue is the winner!", game.winner());
+		    
+	}
+	
+	@Test
+	public void test12_RedWinsInDiagonalWithWinningVariantB(){
+		Linea game = new Linea(7, 6, 'B');
+		game.playRedAt(1);
+		game.playBlueAt(2);
+		game.playRedAt(2);
+		game.playBlueAt(3);
+		game.playRedAt(3);
+		game.playBlueAt(4);
+		game.playRedAt(3);
+		game.playBlueAt(4);
+		game.playRedAt(4);
+		game.playBlueAt(6);
+		game.playRedAt(4);
+		assertTrue(game.finished());
+		assertEquals("Red is the winner!", game.winner());
+		    
+	}
+	@Test
+	public void test13_RedWinsHorizontallyWithWinningVariantc(){
 		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(1);
 		game.playBlueAt(1);
@@ -132,12 +183,12 @@ public class LineaTest {
 		game.playBlueAt(1);
 		game.playRedAt(4);
 		assertTrue(game.finished());
-//		assertEquals("Red is the winner!", game.winner());
-
+		assertEquals("Red is the winner!", game.winner());
+		    
 	}
-
+	
 	@Test
-	public void test11_BlueWinsVertically() {
+	public void test14_BlueWinsVerticallyWithWinningVariantC(){
 		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(2);
 		game.playBlueAt(1);
@@ -148,12 +199,12 @@ public class LineaTest {
 		game.playRedAt(2);
 		game.playBlueAt(1);
 		assertTrue(game.finished());
-//		assertEquals("Blue is the winner!", game.winner());
-
+		assertEquals("Blue is the winner!", game.winner());
+		    
 	}
-
+	
 	@Test
-	public void test12_RedWinsInDiagonal() {
+	public void test15_RedWinsInDiagonalWithWinningVariantC(){
 		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(1);
 		game.playBlueAt(2);
@@ -164,15 +215,15 @@ public class LineaTest {
 		game.playRedAt(3);
 		game.playBlueAt(4);
 		game.playRedAt(4);
-		game.playBlueAt(1);
+		game.playBlueAt(6);
 		game.playRedAt(4);
 		assertTrue(game.finished());
-//		assertEquals("Red is the winner!", game.winner());
-
+		assertEquals("Red is the winner!", game.winner());
+		    
 	}
-
+	
 	@Test
-	public void test13_GameEndedInATie() {
+	public void test16_GameEndedInATie() {
 		Linea game = new Linea(3, 3, 'C');
 		game.playRedAt(1);
 		game.playBlueAt(1);
@@ -184,11 +235,11 @@ public class LineaTest {
 		game.playBlueAt(3);
 		game.playRedAt(3);
 		assertTrue(game.finished());
-//		assertEquals("The game ended in a tie!", game.winner());
+		assertEquals("The game ended in a tie!", game.winner());
 	}
 	
 	@Test
-	public void test14_GameEndedButRedTriesToPlay() {
+	public void test17_GameEndedButRedTriesToPlay() {
 		Linea game = new Linea(7, 6, 'C');
 		game.playRedAt(2);
 		game.playBlueAt(1);
@@ -201,6 +252,24 @@ public class LineaTest {
 		assertTrue(game.finished());
 		try {
 	        game.playRedAt(3);
+	        fail("Se esperaba una excepción, pero no se lanzó.");
+	    } catch (RuntimeException e) {
+	        assertEquals("Game Over!", e.getMessage());
+	    }
+	}
+	@Test
+	public void test18_GameEndedButBlueTriesToPlay() {
+		Linea game = new Linea(7, 6, 'C');
+		game.playRedAt(1);
+		game.playBlueAt(2);
+		game.playRedAt(1);
+		game.playBlueAt(2);
+		game.playRedAt(1);
+		game.playBlueAt(2);
+		game.playRedAt(1);
+		assertTrue(game.finished());
+		try {
+	        game.playBlueAt(2);
 	        fail("Se esperaba una excepción, pero no se lanzó.");
 	    } catch (RuntimeException e) {
 	        assertEquals("Game Over!", e.getMessage());
