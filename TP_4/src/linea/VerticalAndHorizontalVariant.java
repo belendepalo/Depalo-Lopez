f156d1d2a71d1399cf796308bf31071ef94f80b8
belendepalo <104471058+belendepalo@users.noreply.github.com>
2023-11-06 11:36:17 -0300

    Update VerticalAndHorizontalVariant.java

--- TP_4/src/linea/VerticalAndHorizontalVariant.java
+++ TP_4/src/linea/VerticalAndHorizontalVariant.java
@@ -2,54 +2,57 @@ package linea;
 
 import java.util.List;
 
-public class VerticalAndHorizontalVariant extends WinningVariants {
-	public VerticalAndHorizontalVariant() {
-		super('A');
-	}
-
+public class WinningVariantA extends WinningVariants{
+	public WinningVariantA() {
+        super('A');
+    }
+	
 	@Override
 	public boolean canHandle(char variantIdentifier) {
 		return 'A' == variantIdentifier;
 	}
-
+	
 	@Override
-	public boolean checkWin(char chip, List<List<Character>> board) {
-		return checkHorizontal(chip, board) || checkVertical(chip, board);
-	}
-
+    public boolean checkWin(char chip, List<List<Character>> board) {
+        return checkHorizontal(chip, board) || checkVertical(chip, board);
+    }
+	
 	public boolean checkHorizontal(char chip, List<List<Character>> board) {
-
-		for (int rowIndex = 0; rowIndex < board.size(); rowIndex++) {
-			int count = 0;
-			for (int colIndex = 0; colIndex < board.get(rowIndex).size(); colIndex++) {
-				if (board.get(rowIndex).get(colIndex) == chip) {
-					count++;
-					if (count == 4) {
-						return true;
-					}
-				} else {
-					count = 0;
-				}
-			}
-		}
-		return false;
-	}
-
+	
+	 for (int rowIndex = 0; rowIndex < board.size(); rowIndex++) {
+         int count = 0;
+         for (int colIndex = 0; colIndex < board.get(rowIndex).size(); colIndex++) {
+             if (board.get(rowIndex).get(colIndex) == chip) {
+                 count++;
+                 if (count == 4) {
+                     return true;
+                 }
+             } else {
+                 count = 0;
+             }
+         }
+     }
+     return false;
+ }
+	
 	public boolean checkVertical(char chip, List<List<Character>> board) {
-		for (int colIndex = 0; colIndex < board.get(0).size(); colIndex++) {
-			int count = 0;
-			for (int rowIndex = 0; rowIndex < board.size(); rowIndex++) {
-				if (board.get(rowIndex).get(colIndex) == chip) {
-					count++;
-					if (count == 4) {
-						return true;
-					}
-				} else {
-					count = 0;
-				}
-			}
-		}
-		return false;
-	}
+        for (int colIndex = 0; colIndex < board.get(0).size(); colIndex++) {
+            int count = 0;
+            for (int rowIndex = 0; rowIndex < board.size(); rowIndex++) {
+                if (board.get(rowIndex).get(colIndex) == chip) {
+                    count++;
+                    if (count == 4) {
+                        return true;
+                    }
+                } else {
+                    count = 0;
+                }
+            }
+        }
+        return false;
+    }
 
+
+	
+	
 }
