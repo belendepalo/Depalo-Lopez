dbb7d86d214c7d46e2fde82cb5a15e6ea1697495
belendepalo <104471058+belendepalo@users.noreply.github.com>
2023-11-06 11:35:40 -0300

    Update Turns.java

--- TP_4/src/linea/Turns.java
+++ TP_4/src/linea/Turns.java
@@ -3,28 +3,12 @@ package linea;
 import java.util.List;
 
 public abstract class Turns {
+	protected static String ErrorItsRedsTurn = "It's Red's Turn!";
+	protected static String ErrorItsBluesTurn = "It's Blue's Turn!";
+	
+	public abstract void playBlue(int col, List<List<Character>> board);
+    public abstract void playRed(int col, List<List<Character>> board);
+    
 
-	protected static final String ERROR_ITS_REDS_TURN = "It's Red's Turn!";
-	protected static final String ERROR_ITS_BLUES_TURN = "It's Blue's Turn!";
-	protected static final char RED_CHIP = 'R';
-	protected static final char BLUE_CHIP = 'B';
 
-	public abstract void playRedAt(int col, List<List<Character>> board);
-
-	public abstract void playBlueAt(int col, List<List<Character>> board);
-
-	public abstract Turns nextTurn();
-
-	protected void placeChip(int col, List<List<Character>> board, char chip) {
-		if (board.get(0).get(col - 1) != ' ') {
-			throw new RuntimeException("Column is full!");
-		}
-
-		for (int rowIndex = board.size() - 1; rowIndex >= 0; rowIndex--) {
-			if (board.get(rowIndex).get(col - 1) == ' ') {
-				board.get(rowIndex).set(col - 1, chip);
-				return;
-			}
-		}
-	}
 }
