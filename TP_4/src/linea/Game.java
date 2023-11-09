d8d0e29c9fc12ce10da66e6cf9e33c6c15665e21
belendepalo <104471058+belendepalo@users.noreply.github.com>
2023-11-08 23:35:28 -0300

    Update Game.java
    Correciones Generales

--- TP_4/src/linea/Game.java
+++ TP_4/src/linea/Game.java
@@ -2,29 +2,62 @@ package linea;
 
 public class Game {
 
-	public static void main(String[] args) throws Exception {
-		System.out.println("Dimensiones?");
-		Linea game = new Linea(promptAsInt("Columnas? "), 
-							   promptAsInt("Filas? "),
-				               promptAsChar("Estartegia de Juego: A, B o C? "));
-		System.out.println(game.show());
-		while (!game.finished()) {
-			game.playRedAt(promptAsInt("Rojas? "));
-			System.out.println(game.show());
-			if (!game.finished()) {
-				game.playBlueAt(promptAsInt("Azul? "));
-				System.out.println(game.show());
-			}
-		}
-	}
+	  public static void main( String[] args) throws Exception {
 
-	private static int promptAsInt(String message) {
-		System.out.print(message);
-		return Integer.parseInt(System.console().readLine());
-	}
+	    System.out.println( "Dimensiones?");
 
-	private static char promptAsChar(String message) {
-		System.out.print(message);
-		return System.console().readLine().charAt(0);
+	    LineGame game = new LineGame( promptAsInt( "Base? " ), 
+
+	                            promptAsInt( "Altura? " ), 
+
+	                            promptAsChar( "Estartegia de Juego: A, B o C? " ) );
+
+	    
+
+	    System.out.println( game.show() );
+
+	    
+
+	    while ( !game.finished() ) {
+
+	      game.playRedAt( promptAsInt( "Rojas? " ) );
+
+	      System.out.println( game.show() );
+
+	      
+
+	      if ( !game.finished() ) {
+
+	        game.playBlueAt( promptAsInt( "Azul? " ) );
+
+	        System.out.println( game.show() );
+
+	      }
+	      
+	    }
+	    System.out.println(game.winner());
+	    
+
+	  }
+
+
+
+	  private static int promptAsInt( String message ) {
+
+	    System.out.print( message );
+
+	    return Integer.parseInt( System.console().readLine() );
+
+	  }
+
+	  
+
+	  private static char promptAsChar( String message ) {
+
+	    System.out.print( message );
+
+	    return System.console().readLine().charAt( 0 );
+
+	  }
+
 	}
-}
