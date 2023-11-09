16612ff8c2a60d6e96370187ff1607105db23573
belendepalo <104471058+belendepalo@users.noreply.github.com>
2023-11-08 23:34:44 -0300

    Update BluesTurn.java
    Correciones Generales

--- TP_4/src/linea/BluesTurn.java
+++ TP_4/src/linea/BluesTurn.java
@@ -1,20 +1,25 @@
 package linea;
-import java.util.List;
-import java.util.stream.IntStream;
 
-public class BluesTurn extends Turns{
-    @Override
-    public void playBlue(int col, List<List<Character>> board) {
-        // Colocar la ficha 'B' en la posición correspondiente
-    	 IntStream.rangeClosed(0, board.size() - 1)
-         .filter(rowIndex -> board.get(board.size() - 1 - rowIndex).get(col - 1) == ' ')
-         .findFirst()
-         .ifPresent(rowIndex -> board.get(board.size() - 1 - rowIndex).set(col - 1, 'B'));
-    }
-        
+public class BluesTurn extends GameStatus{
+
 	@Override
-	public void playRed(int col, List<List<Character>> board) {
-        throw new RuntimeException(ErrorItsBluesTurn);
-    }
+	public void playRedAt(LineGame game, int column) {
+		throw new RuntimeException("It's Blue's Turn!");		
+	}
 
+	@Override
+	public void playBlueAt(LineGame game, int column) {
+		game.placeChip('B', column);		
+	}
+
+	@Override
+	public boolean finished() {
+		return false;
+	}
+
+	@Override
+	public String StatusOfGame() {
+		return "The game is still ongoing.";
+	}
+	
 }
