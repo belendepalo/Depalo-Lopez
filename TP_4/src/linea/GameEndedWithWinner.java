0b6af3749d0c94e5f921c461304e194917f6ac48
belendepalo <104471058+belendepalo@users.noreply.github.com>
2023-11-08 23:37:24 -0300

    Update and rename GameOver.java to GameEndedWithWinner.java
    Correciones Generales

---
+++ TP_4/src/linea/GameEndedWithWinner.java
@@ -0,0 +1,26 @@
+package linea;
+
+public class GameEndedWithWinner extends GameStatus{
+
+	@Override
+	public void playRedAt(LineGame game, int column) {
+		throw new RuntimeException("Game Over!");		
+		
+	}
+
+	@Override
+	public void playBlueAt(LineGame game, int column) {
+		throw new RuntimeException("Game Over!");			
+	}
+
+	@Override
+	public boolean finished() {
+		return true;
+	}
+
+	@Override
+	public String StatusOfGame() {
+		return "You won the game!";
+	}
+
+}
