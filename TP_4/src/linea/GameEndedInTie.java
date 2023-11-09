227f03083c76ed47d5572ddd53b26c7bbef3d141
belendepalo <104471058+belendepalo@users.noreply.github.com>
2023-11-08 23:36:23 -0300

    Update and rename OngoingGame.java to GameEndedInTie.java
    Correciones Generales

---
+++ TP_4/src/linea/GameEndedInTie.java
@@ -0,0 +1,25 @@
+package linea;
+
+public class GameEndedInTie extends GameStatus{
+
+	@Override
+	public void playRedAt(LineGame game, int column) {
+		throw new RuntimeException("Game Over!");			
+	}
+
+	@Override
+	public void playBlueAt(LineGame game, int column) {
+		throw new RuntimeException("Game Over!");			
+	}
+
+	@Override
+	public boolean finished() {
+		return true;
+	}
+
+	@Override
+	public String StatusOfGame() {
+		return "The game ended in a tie!";
+	}
+
+}
